89972c8d6129276d4ac9a980643ee13a8d645a80
Tuan Le <tuandle@users.noreply.github.com>
2020-09-28 10:30:32 +0200

    Update utils.py
    add save trained model

--- utils.py
+++ utils.py
@@ -9,6 +9,8 @@ from detectron2.engine import DefaultTrainer, DefaultPredictor
 from detectron2.config import get_cfg
 from detectron2.data import MetadataCatalog
 from detectron2.data.datasets import register_coco_instances, load_coco_json
+from detectron2.checkpoint import DetectionCheckpointer
+from detectron2.modeling import build_model
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -69,12 +71,11 @@ def train_model(dataset):
     cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url('COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml')  # Let training initialize from model zoo
     cfg.SOLVER.IMS_PER_BATCH = 4 # 4
     cfg.SOLVER.BASE_LR = 0.00025  # pick a good LR
-    cfg.SOLVER.MAX_ITER = 6000    # 300 iterations seems good enough for this toy dataset; you may need to train longer for a practical dataset
+    cfg.SOLVER.MAX_ITER = 6    # 300 iterations seems good enough for this toy dataset; you may need to train longer for a practical dataset
     cfg.MODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE = 512   # faster, and good enough for this toy dataset (default: 512)
     cfg.MODEL.ROI_HEADS.NUM_CLASSES = len(dataset.categories)  # number of categories
 #     cfg.MODEL.DEVICE = 'cuda'
     print('Max iter is ',cfg.SOLVER.MAX_ITER)
-    print('Current path is ',cfg.OUTPUT_DIR)
     # Start the training
     os.makedirs(cfg.OUTPUT_DIR, exist_ok=True)
     trainer = DefaultTrainer(cfg) 
@@ -86,6 +87,12 @@ def train_model(dataset):
     cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.7   # set the testing threshold for this model
     cfg.DATASETS.TEST = ('my_dataset', )
     cfg.TEST.DETECTIONS_PER_IMAGE = 1000
+    
+    built_model = build_model(cfg)  # returns a torch.nn.Module
+    DetectionCheckpointer(built_model).load(cfg.MODEL.WEIGHTS) #capture trained model
+    checkpointer = DetectionCheckpointer(built_model, save_dir="/content/gdrive/My Drive/Colab Notebooks")
+    checkpointer.save("model_final")  # save to output/model_999.pth
+    
     predictor = DefaultPredictor(cfg)
     model = Model(predictor)
 
